6ffef2da36b658b5558ed1b5178ac0da35afc317
rafet289 <79151705+rafet289@users.noreply.github.com>
2026-08-20 14:20:15 +0400

    Update MainActivity.kt

--- app/src/main/java/com/via7/samsunganim/tv/MainActivity.kt
+++ app/src/main/java/com/via7/samsunganim/tv/MainActivity.kt
@@ -2,11 +2,13 @@ package com.via7.samsunganim.tv
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.via7.samsunganim.databinding.ActivityMainTvBinding
-class MainActivity:AppCompatActivity(){
-private lateinit var b:ActivityMainTvBinding
-override fun onCreate(s:Bundle?){super.onCreate(s); b=ActivityMainTvBinding.inflate(layoutInflater); setContentView(b.root)
-b.btnBrowser.setOnClickListener{ startActivity(Intent(this, BrowserActivity::class.java)) }
-b.btnYoutube.setOnClickListener{ val i=Intent(this, BrowserActivity::class.java); i.putExtra("url","https://www.youtube.com"); startActivity(i) }
-}
+import com.via7.samsunganim.tv.databinding.ActivityMainTvBinding
+
+class MainActivity: AppCompatActivity(){
+    private lateinit var b: ActivityMainTvBinding
+    override fun onCreate(s: Bundle?){super.onCreate(s); b=ActivityMainTvBinding.inflate(layoutInflater); setContentView(b.root)
+        b.btnBrowser.setOnClickListener{ startActivity(Intent(this, BrowserActivity::class.java)) }
+        b.btnYoutube.setOnClickListener{ val i=Intent(this, BrowserActivity::class.java); i.putExtra("url","https://www.youtube.com"); startActivity(i) }
+        b.btnSettings.setOnClickListener{ finish() }
+    }
 }
